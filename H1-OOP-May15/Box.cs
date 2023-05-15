59c3ed5db0428d80c6b76245d7aca8b74acbd0e3
Allan <AGawron2021@protonmail.com>
2023-05-15 20:24:22 +0200

    Add project files.

---
+++ H1-OOP-May15/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_OOP_May15
+{
+    public class Box
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        enum BoxType
+        {
+            smallBox,
+            mediumBox,
+            largeBox
+        }
+        public Box(double height, double width, double length)
+        {
+            Height = height;
+            Width = width;
+            Length = length;
+        }
+    }
+}
---
+++ H1-OOP-May15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_OOP_May15
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Box B4 = new Box(5, 4, 2); //smallBox
+            Box C1 = new Box(3, 5, 2); //smallBox
+            Box M5 = new Box(6, 2, 3); //mediumBox
+            Box N9 = new Box(1, 8, 4); //largeBox
+
+            double B4Volume = GetVolume(B4.Height, B4.Width, B4.Length);
+            Console.WriteLine(B4Volume);
+
+            Console.ReadKey();
+        }
+        public static double GetVolume(double Height, double Width, double Length)
+        {
+            double Result = Height * Width * Length;
+            return (Result);
+        }
+    }
+}
+
+
+//
+//
+//
+//Regel:
+//LilleBox + lilleBox giver mellemBox, resten er storBox.
+//Siderne i den nye box skal beregnes, og tildeles det nye objekt, indregn et tab på 5% i de nye værdier
+//Box G55 = B4 + C1; // B4(smallBox) + C1(smallBox) = G55(mediumBox) 
